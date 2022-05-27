13da7be8f7c78eb2eb43b04d8b47e01a90f11619
rubensantibanezacosta <rubensantibanezacosta902@gmail.com>
2022-05-27 17:19:37 +0100

    setting diferrent endpoint

--- src/test/java/com/name/server/cxf/TempConverterEndpointTest.java
+++ src/test/java/com/name/server/cxf/TempConverterEndpointTest.java
@@ -2,16 +2,23 @@ package com.name.server.cxf;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.ws.BindingProvider;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TempConverterEndpointTest {
 
     @Test
     void celsiusToFahrenheit() {
+
         TempConverterEndpoint service = new TempConverterEndpointService().getTempConverterEndpointPort();
+        String url = "http://learnwebservices.com/services/tempconverter";
+        BindingProvider bindingProvider = (BindingProvider) service;
+        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
         CelsiusToFahrenheitRequest request = new CelsiusToFahrenheitRequest();
         request.setTemperatureInCelsius(0);
         CelsiusToFahrenheitResponse response = service.celsiusToFahrenheit(request);
+        System.out.println(response.getTemperatureInFahrenheit());
         assertEquals(response.getTemperatureInFahrenheit(), 32.0);
     }
 
